2bd5cf90d444638b7a73a1e60e7455200b0e9037
Jordan Theovady Ferdinand <jordan.ferdinand@intern.indomobil.co.id>
2026-09-21 14:12:58 +0700

    formatting with biome

--- src/ping.js
+++ src/ping.js
@@ -1,19 +1,19 @@
-import http from 'k6/http';
-import {sleep, check} from 'k6';
+import { check, sleep } from "k6";
+import http from "k6/http";
 
 export const options = {
-    vus: 10,
-    // duration: '30s',
-    stages: [
-        {duration: '10s', target: 20},
-        {duration: '10s', target: 10},
-        {duration: '10s', target: 0},
-    ],
-    summaryTrendStats: ['avg', 'min', 'med', 'max', 'p(90)', 'p(95)', 'p(99)']
+	vus: 10,
+	// duration: '30s',
+	stages: [
+		{ duration: "10s", target: 20 },
+		{ duration: "10s", target: 10 },
+		{ duration: "10s", target: 0 },
+	],
+	summaryTrendStats: ["avg", "min", "med", "max", "p(90)", "p(95)", "p(99)"],
 };
 
 export default function () {
-    let res = http.get('http://localhost:6767/api/v1/products');
-    check(res, {"status is 200": (res) => res.status === 200});
-    sleep(1);
+	const res = http.get("http://localhost:6767/api/v1/products");
+	check(res, { "status is 200": (res) => res.status === 200 });
+	sleep(1);
 }
--- src/register.js
+++ src/register.js
@@ -1,51 +1,64 @@
-import http from 'k6/http';
-import {sleep, check, fail} from 'k6';
+import { check, fail, sleep } from "k6";
+import http from "k6/http";
 
 export const options = {
-    vus: 10, duration: '10s',
+	vus: 10,
+	duration: "10s",
 };
 
 export default function () {
-    const uniqueId = new Date().getTime()
-    const body = {
-        username: `user ${uniqueId}`, password: `rahasia`, name: `roger mr`
-    }
+	const uniqueId = new Date().getTime();
+	const body = {
+		username: `user ${uniqueId}`,
+		password: `rahasia`,
+		name: `roger mr`,
+	};
 
-    const res = http.post('http://localhost:3000/api/users', JSON.stringify(body), {
-        headers: {
-            'Accept': 'application/json', 'Content-Type': 'application/json',
-        }
-    })
+	const res = http.post(
+		"http://localhost:3000/api/users",
+		JSON.stringify(body),
+		{
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+			},
+		},
+	);
 
-    if (res.status !== 200) {
-        fail(res.error)
-    }
+	if (res.status !== 200) {
+		fail(res.error);
+	}
 
-    const loginBody = {
-        username: `user ${uniqueId}`,
-        password: 'rahasia',
-    }
+	const loginBody = {
+		username: `user ${uniqueId}`,
+		password: "rahasia",
+	};
 
-    const response = http.post('http://localhost:3000/api/users/login', JSON.stringify(loginBody), {
-        headers: {
-            'Accept': 'application/json', 'Content-Type': 'application/json',
-        }
-    })
+	const response = http.post(
+		"http://localhost:3000/api/users/login",
+		JSON.stringify(loginBody),
+		{
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+			},
+		},
+	);
 
-    if (response.status !== 200) {
-        fail(response.error)
-    }
+	if (response.status !== 200) {
+		fail(response.error);
+	}
 
-    const responseBody = response.json()
+	const responseBody = response.json();
 
-    const currentResponse = http.get('http://localhost:3000/api/users/current', {
-        headers: {
-            'Accept': 'application/json',
-            'Authorization': responseBody.data.token,
-        }
-    });
+	const currentResponse = http.get("http://localhost:3000/api/users/current", {
+		headers: {
+			Accept: "application/json",
+			Authorization: responseBody.data.token,
+		},
+	});
 
-    if (currentResponse.status !== 200) {
-        fail(currentResponse.error)
-    }
+	if (currentResponse.status !== 200) {
+		fail(currentResponse.error);
+	}
 }
